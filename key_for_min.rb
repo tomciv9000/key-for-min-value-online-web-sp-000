92cf46d238ea9daae87fcbe25b19598768d18a2f
Tom White <eugene.white@gmail.com>
2019-05-26 23:00:37 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,5 +5,8 @@ def key_for_min_value(name_hash)
   key_for_min = nil
   lowest_value = nil 
   name_hash.each do |key, value|
-    
+    if value == nil
+      key_for_min = key
+      lowest_value = value
+    elsif value 
 end
